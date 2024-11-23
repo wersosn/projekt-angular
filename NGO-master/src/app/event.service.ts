9d3e3ae35e527de8fdb185023f845ef903387908
pawelsiemieniuk <pawel.siemieniuk11@gmail.com>
2024-11-23 20:06:21 +0100

    Nowe interfejsy 'Group' i 'User' do nowej struktury wydarzeń. Nowe funkcje do pobierania i edycji grup z backendu (dodawanie do skończenia)

--- NGO-master/src/app/event.service.ts
+++ NGO-master/src/app/event.service.ts
@@ -3,6 +3,23 @@ import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
+// Interfejs User
+export interface User {
+  id: number;
+  name: string;
+  surname: string;
+}
+
+// Interfejs Group
+export interface Group {
+  id: number;
+  type: string;
+  name: string;
+  size: number;
+  time: string;
+  users: User[];
+}
+
 // Interfejs Event
 export interface Event {
   id: number;
@@ -12,6 +29,7 @@ export interface Event {
   time: string;
   location: string;
   seats: number;
+  groups: Group[];
 }
 
 @Injectable({
@@ -20,6 +38,7 @@ export interface Event {
 
 export class EventService {
   private eventsUrl = 'http://localhost:3000/api/events'; // Endpoint API
+  private groupsUrl = 'groups';
   private eventsSubject = new BehaviorSubject<Event[]>([]); // Strumień wydarzeń
   events$ = this.eventsSubject.asObservable(); // Subskrybowalne dane
 
@@ -51,7 +70,32 @@ export class EventService {
   // Aktualizacja wydarzenia
   updateEvent(eventId: number, eventData: any): Observable<Event> {
     return this.http.put<Event>(`http://localhost:3000/api/events/${eventId}`, eventData);
-  }  
+  } 
+
+  // Pobranie grup dla konkretnego wydarzenia na podstawie ID (synchronicznie z BehaviorSubject)
+  getGroups(eventId: number): Group[] | undefined {
+    return this.eventsSubject.value.find(e => e.id === eventId)?.groups;
+  }
+  // Pobieranie konkretnej grupy dla konkretnego wydarzenia na podstawie ID
+  getGroupById(eventId: number, groupId: number): Observable<Group> {
+    return this.http.get<Group>(`${this.eventsUrl}/${eventId}/${this.groupsUrl}/${groupId}`);
+  }
+  // Dodanie nowego wydarzenia do serwera
+  addGroup(eventId: number, group: Group): Observable<Group> {
+    return this.http.post<Group>(`${this.eventsUrl}/${eventId}/${this.groupsUrl}`, group).pipe(
+      tap(newGroup => {
+        const currentEventGroups = this.eventsSubject.value.find(e => e.id === eventId)?.groups;
+        // TODO Dodać grupę do eventu
+        // Nie wiem jak to zrobić, może jak tego nie poprawicie i wszystko skończe to usiąde nad tym
+        //this.eventsSubject.next([...currentEventGroups, newGroup]);
+      })
+    );
+  }
+  // Aktualizacja grupy
+  updateGroup(eventId: number, groupId: number, groupData: any): Observable<Event> {
+    return this.http.put<Event>(`${this.eventsUrl}/${eventId}/${this.groupsUrl}/${groupId}`, groupData);
+  } 
+
 }
 
 // Stary kod
