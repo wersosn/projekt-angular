6198ea48515e0b39347c73043a7e3fd84a85bcff
pawelsiemieniuk <pawel.siemieniuk11@gmail.com>
2024-11-23 20:09:09 +0100

    Pobieranie wydarzeń z backendu i wyświetlanie w kalendarzu. Przejście do szczegółów wydarzenia po naciśnięciu na nie.

--- NGO-master/src/app/kalendarz-akcji/kalendarz/kalendarz.component.ts
+++ NGO-master/src/app/kalendarz-akcji/kalendarz/kalendarz.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { KalendarzAkcjiComponent } from '../kalendarz-akcji.component';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
 import { CalendarOptions } from '@fullcalendar/core'; 
+import interactionPlugin from '@fullcalendar/interaction';
 import { FullCalendarModule } from '@fullcalendar/angular';
+import { EventService } from '../../event.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-kalendarz',
   standalone: true,
@@ -10,9 +14,44 @@ import { FullCalendarModule } from '@fullcalendar/angular';
   templateUrl: './kalendarz.component.html',
   styleUrl: './kalendarz.component.scss'
 })
-export class KalendarzComponent {
+// TODO Zmienić kolejność dni tygodnia (na normalną)
+// TODO Dodać zmianę view? na widok jednego dnia w handleDateClick
+// TODO W przyszłości filtry na wszystkie wydarzenia i te w których użytkownik bierze udział
+export class KalendarzComponent implements OnInit{
+  events: any[] = [];
+  calendarEvents: any[] = [];
+
   calendarOptions: CalendarOptions = {
-    initialView: 'dayGridMonth',
-    plugins: [dayGridPlugin]
-  };
+      initialView: 'dayGridMonth',
+      plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+      dateClick: (arg) => this.handleDateClick(arg),
+      eventClick: (arg) => this.handleEventClick(arg),
+      events: [ ]
+    };
+
+  // Konstruktor, w którym wstrzykiwany jest serwis EventService i Router
+  constructor(private eventService: EventService, private router: Router) { }
+  
+  ngOnInit(): void {
+    this.eventService.fetchEvents(); // Pobieranie wydarzenia z serwera
+    this.eventService.events$.subscribe(events => { // Przypisanie pobranych wydarzeń do lokalnej tablicy events
+      this.events = events;
+      this.calendarEvents = [];
+
+      this.events.forEach(event => {
+        this.calendarEvents.push({id: event.id, title: event.title, date: event.date, color: event.color});
+      });
+      this.calendarOptions.events = [...this.calendarEvents];
+    });
+
+  }
+
+  handleDateClick(date:any) {
+    console.log(date);
+  }
+
+  handleEventClick(event:any) {
+    this.router.navigateByUrl(`szczegóły-akcji/${event.event._def.publicId}`)
+  }
+  
 }
