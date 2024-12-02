a8e733a4cbee8266de4a671a77ab681799cb5858
Tomaszek <tomaszm2021@gmail.com>
2024-12-02 15:45:50 +0100

    sortowanie i troche filtrowania

--- NGO-master/src/app/blog-akcji/blog-akcji.component.ts
+++ NGO-master/src/app/blog-akcji/blog-akcji.component.ts
@@ -12,22 +12,68 @@ import { RouterModule, Routes } from '@angular/router';
   templateUrl: './blog-akcji.component.html',
   styleUrl: './blog-akcji.component.scss'
 })
-
 export class BlogAkcjiComponent implements OnInit {
-  events: any[] = []; // Eventy
+  events: any[] = [];
+  allEvents: any[] = [];
+  isFiltered: boolean = false;
 
-  // Konstruktor, w którym wstrzykiwany jest serwis EventService
+  sortDirectionDate: 'asc' | 'desc' = 'asc';
+  sortDirectionSeats: 'asc' | 'desc' = 'asc';
+
+  activeSortByDate: boolean = false;
+  activeSortBySeats: boolean = false;
+
   constructor(private eventService: EventService) { }
 
   ngOnInit() {
-    this.eventService.fetchEvents(); // Pobieranie wydarzenia z serwera
-    this.eventService.events$.subscribe(events => { // Przypisanie pobranych wydarzeń do lokalnej tablicy events
+    this.fetchAndSetEvents();
+  }
+
+  fetchAndSetEvents() {
+    this.eventService.fetchEvents();
+    this.eventService.events$.subscribe(events => {
       this.events = events;
+      this.allEvents = [...events];
     });
   }
 
-  // Funkcja trackBy, która wykorzystuje unikalny identyfikator (id) elementu
+  sortByDate() {
+    this.events.sort((a, b) => {
+      const diff = new Date(a.date).getTime() - new Date(b.date).getTime();
+      return this.sortDirectionDate === 'asc' ? diff : -diff;
+    });
+    this.sortDirectionDate = this.sortDirectionDate === 'asc' ? 'desc' : 'asc';
+    this.activeSortByDate = true;
+    this.activeSortBySeats = false; // Dezaktywuj sortowanie po miejscach
+  }
+
+  sortBySeats() {
+    this.events.sort((a, b) => {
+      const diff = a.seats - b.seats;
+      return this.sortDirectionSeats === 'asc' ? diff : -diff;
+    });
+    this.sortDirectionSeats = this.sortDirectionSeats === 'asc' ? 'desc' : 'asc';
+    this.activeSortBySeats = true;
+    this.activeSortByDate = false; // Dezaktywuj sortowanie po dacie
+  }
+
+  resetOrder() {
+    this.events = [...this.allEvents];
+    this.activeSortByDate = false;
+    this.activeSortBySeats = false; // Resetuj wszystkie sortowania
+  }
+
+  toggleFilterSeats() {
+    if (this.isFiltered) {
+      this.events = [...this.allEvents];
+    } else {
+      this.events = this.allEvents.filter(event => event.seats > 0);
+    }
+    this.isFiltered = !this.isFiltered;
+  }
+
   trackById(index: number, event: any): number {
     return event.id;
   }
 }
+
